563cf37b9af36f572cc237400ad6cc2c2076047c
NajlaAbassi <abassi.nejla96@gmail.com>
2024-10-12 16:29:51 +0200

    fix check for system type for processfastq function to trigger the right message

--- R/processfastq.R
+++ R/processfastq.R
@@ -1,1 +1,4 @@
 #' @param paired A logical indicating whether the data is single or pair-end.
+    if(Sys.info()[['sysname']] != "Linux"){
+    stop("This function is only available on Linux.")
+    }
