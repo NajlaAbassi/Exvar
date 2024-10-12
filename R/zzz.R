80840ce9c85db227c649a399400aa206631afacb
NajlaAbassi <abassi.nejla96@gmail.com>
2024-10-12 16:35:10 +0200

    update the startup msg, added processfastq function to the list of non functional functions

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(paste0("This package was created with the Linux operating system in mind. ",
+                        "Functions processfastq(), callcnv(), callindels(), callsnp(), count(), and geneExpression() are nonfunctional on other operating systems.\n",
+                              "Additionally, a samtools installation is recommended for snp and indel calling."))
+}
